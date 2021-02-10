565e63b5ae2b9f1ec29da9da02f7e46f3fa470f0
MCGonzalez831 <mirngonzalez@csumb.edu>
2021-02-09 21:32:27 -0800

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,16 @@
+
+	//Get a deep saucepan
+	//Put eggs in saucepan
+	//Fill saucepan with water unitl eggs are submered
+	//Turn on heat on medium-high
+	//Let eggs boil for 10-12 minutes
+	//After boiled, drain hot water and run under cold water
+	//Peel shells under cold water
+	//And Viola! Now you have boiled eggs
+
+
+  //display maze path
+  //detect player input
+  //move based on input
+ //display new paths possible
+ //move through maze unil goal reached
