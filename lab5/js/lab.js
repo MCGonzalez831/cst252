88d70a8516c2d2ab9317c38eb7011e59e4035343
MCGonzalez831 <mirngonzalez@csumb.edu>
2021-02-12 22:47:24 -0800

    lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+// lab5.js - Play with variables and stuff
+// Author: Mirna Gonzalez <mirngonzalez@ucsc.edu>
+// Created: 12 Febuary
+// License: Public Domain
+// I am very confused why is it not showing up pls help
+
+
+// Define variables
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
+
+// Calculate
+age = 2019 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
