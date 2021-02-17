4a9b3df6fdaade4d99a170d57a0db986ea687733
MCGonzalez831 <mirngonzalez@csumb.edu>
2021-02-16 23:08:47 -0800

    lab6

---
+++ lab6/js/lab.js
@@ -0,0 +1,24 @@
+// lab6.js - Arrays and Objects
+// Author: Mirna Gonzalez <mirngonzalez@ucsc.edu>
+// Created: 16 Febuary
+// License: Public Domain
+
+// Difine Variables
+
+      myTransport = ["Nissan", "Versa note", "rides from friend",];
+
+myMainRide = {
+	make: "Nissan",
+	model: "Versa note",
+	color: "white",
+	year: 2015,
+	age: function()
+	{ return 2020 - age;
+
+	}
+}
+
+//output
+document.writeln("Kinds of Transportion I use:", myTransport, "</br>");
+document.writeln("My Main Ride: <pre>",
+	    JSON.stringify(myMainRide, null, '\t'),"</pre>");
