4f31b775e55df4f8bd6082e82104dcec83c30cdf
MCGonzalez831 <mirngonzalez@csumb.edu>
2021-03-02 20:51:02 -0800

    lab9update

---
+++ lab9/js/lab.js
@@ -0,0 +1,23 @@
+// lab8.js
+// Author: Mirna Gonzalez <mirngonzalez@ucsc.edu>
+// Created: 1 March
+// License: Public Domain
+
+var outputE1 =
+document.getElementById("output");
+
+// First Element
+var new1E1 =
+document.createElement("p");
+  new1E1.innerHTML = "Please look at this thing";
+
+// Second Element
+var new2E1 =
+document.createElement("p");
+ new2E1.innerHTML = "second thing";
+
+ //appends
+
+ document.body.appendChild(new1E1)
+
+ document.body.appendChild(new2E1)
