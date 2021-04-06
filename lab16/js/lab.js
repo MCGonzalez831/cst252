0df96fa5735077dab495e917a4b672ed02c8fa18
MCGonzalez831 <mirngonzalez@csumb.edu>
2021-04-06 11:52:37 -0700

    lab16

---
+++ lab16/js/lab.js
@@ -0,0 +1,15 @@
+/* lab.js
+ *Author: Mirna Gonzalez <mirngonzalez@ucsc.edu>
+ *Created:  5 April
+ *License: Public Domain
+ */
+
+//Also I know noone cares but Stone Ocean got annouced 
+
+ $(document).ready(function(){
+   $("button").click(function(){
+     alert("Hey it worked! Cool! Now what? you look at my page that's what. :D");
+   });
+ });
+
+ $("button").css("color","black");
