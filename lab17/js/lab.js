4ae088e8f54f9fdce4124303e8c93a7a20c9e0e3
MCGonzalez831 <mirngonzalez@csumb.edu>
2021-04-08 15:04:30 -0700

    lab17

---
+++ lab17/js/lab.js
@@ -0,0 +1,26 @@
+/* lab.js
+ *Author: Mirna Gonzalez <mirngonzalez@ucsc.edu>
+ *Created:  7 April
+ *License: Public Domain
+ */
+
+$(document).ready(function(){
+  $("#navy-button").click(function(){
+      $("#navy").css({"color": "Navy"});},
+  );
+});
+
+    $(document).ready(function(){
+      $("#blue-button").click(function(){
+          $("#blue").css({"color": "blue"});},
+      );
+    });
+
+
+      $(document).ready(function(){
+        $("#lightblue-button").click(function(){
+            $("#lightblue").css({"color": "lightblue"});},
+        );
+      });
+
+//Also no fancy background, I am tired
