66e7b03d35c16a7dbd8a997bdae3d210ea87ba94
MCGonzalez831 <mirngonzalez@csumb.edu>
2021-04-13 13:34:45 -0700

    lab update

---
+++ lab18/js/lab.js
@@ -0,0 +1,8 @@
+/* lab.js
+ *Author: Mirna Gonzalez <mirngonzalez@ucsc.edu>
+ *Created:  13 April
+ *License: Public Domain
+ */
+
+ //I just wanna vent on here
+ //What do you do when people want to be freinds again but you don't want to?
